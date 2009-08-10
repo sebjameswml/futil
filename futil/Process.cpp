d4fa1b5643e125af96a4d0e813227ec98323d5c7
sebjameswml <sjames@wmltd.co.uk>
2009-08-10 09:22:40 +0000

    Added the rather useful and neat Process class, which I originally wrote for nxcl.
    git-svn-id: svn+ssh://s1/data/svnroot/wml/futil@1982 f8f6422b-730d-0410-8a4c-f5298e861a9a

---
+++ futil/Process.cpp
@@ -0,0 +1,305 @@
+/*
+ * A class to fork and exec processes.
+ *
+ * Copyright 2009, William Matthew Limited.
+ *
+ * Author: S James <sjames@wmltd.co.uk>
+ */
+
+#include <iostream>
+#include <sstream>
+extern "C" {
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/poll.h>
+#include <signal.h>
+}
+
+#include "config.h"
+#include "Process.h"
+
+using namespace std;
+using namespace wml;
+
+/*!
+ * Implementation of the Process class
+ */
+//@{
+
+// Used when dealing with pipes
+#define READING_END 0
+#define WRITING_END 1
+#define STDIN  0
+#define STDOUT 1
+#define STDERR 2
+
+// Constructor
+Process::Process () :
+	progName("unknown"),
+	error (PROCESSNOERROR),
+	pid(0),
+	signalledStart(false)
+{
+	// Set up the polling structs
+	this->p = static_cast<struct pollfd*>(malloc (2*sizeof (struct pollfd)));
+}
+
+// Destructor
+Process::~Process ()
+{
+	free (this->p);
+}
+
+void
+Process::writeIn (string& input)
+{
+	write (this->parentToChild[WRITING_END], input.c_str(), input.size());
+}
+
+// fork and exec a new process using execv, which takes stdin via a
+// fifo and returns output also via a fifo.
+int
+Process::start (const string& program, const list<string>& args)
+{
+	char** argarray;
+	list<string> myargs = args;
+	list<string>::iterator i;
+	unsigned int j = 0;
+	int theError;
+
+	// NB: The first item in the args list should be the program name.
+	this->progName = program;
+
+	// Set up our pipes
+	if (pipe(parentToChild) == -1 || pipe(childToParent) == -1 || pipe(childErrToParent) == -1) {
+		return PROCESS_FAILURE;
+	}
+
+	this->pid = fork();
+	switch (this->pid) {
+	case -1:
+		return PROCESS_FAILURE;
+	case 0:
+		// This is the CHILD process
+
+		// Close unwanted ends of the pipes
+		close (parentToChild[WRITING_END]);
+		close (childToParent[READING_END]);
+		close (childErrToParent[READING_END]);
+
+		// Now all we have to do is make the writing file
+		// descriptors 0,1 or 2 and they will be used instead
+		// of stdout, stderr and stdin.
+		if ((dup2 (parentToChild[READING_END], STDIN)) == -1  ||
+		    (dup2 (childToParent[WRITING_END], STDOUT)) == -1 ||
+		    (dup2 (childErrToParent[WRITING_END], STDERR)) == -1) {
+			theError = errno;
+			cout << "ERROR! Couldn't get access to stdin/out/err! errno was " << theError << endl;
+			return PROCESS_FAILURE;
+		}
+
+		// Allocate memory for the program arguments
+		// 1+ to allow space for NULL terminating pointer
+		argarray = static_cast<char**>(malloc ((1+args.size()) * sizeof (char*)));
+		for (i=myargs.begin(); i!=myargs.end(); i++) {
+			argarray[j] = static_cast<char*>(malloc ( (1+(*i).size()) * sizeof (char) ));
+			snprintf (argarray[j++], 1+(*i).size(), "%s", (*i).c_str());
+			dbgln(*i);
+		}
+		argarray[j] = NULL;
+
+		dbgln ("About to execute '" + program + "' with those arguments..");
+
+		execv (program.c_str(), argarray);
+
+		// If process returns, error occurred
+		theError = errno;
+		// This'll get picked up by parseResponse
+		cout << "Process error: " << this->pid << " crashed. errno:" << theError << endl;
+
+		// This won't get picked up by the parent process.
+		this->error = PROCESSCRASHED;
+
+		// In this case, we close the pipes to signal to the parent that we crashed
+		close (parentToChild[READING_END]);
+		close (childToParent[WRITING_END]);
+		close (childErrToParent[WRITING_END]);
+
+		// Child should exit now.
+		_exit(-1);
+
+	default:
+		// This is the PARENT process
+
+		// Close unwanted ends of the pipes
+		close (parentToChild[READING_END]);
+		close (childToParent[WRITING_END]);
+		close (childErrToParent[WRITING_END]);
+
+		// Write to this->parentToChild[WRITING_END] to write to stdin of the child
+		// Read from this->childToParent[READING_END] to read from stdout of child
+		// Read from this->childErrToParent[READING_END] to read from stderr of child
+
+		break;
+	}
+	return PROCESS_MAIN_APP;
+}
+
+
+// If no pid after a while, return false.
+bool
+Process::waitForStarted (void)
+{
+	unsigned int i=0;
+	while (this->pid == 0 && i<1000) {
+		usleep (1000);
+		i++;
+	}
+	if (this->pid>0) {
+		dbgln ("The process started!");
+		this->callbacks->startedSignal (this->progName);
+		this->signalledStart = true;
+		return true;
+	} else {
+		this->error = PROCESSFAILEDTOSTART;
+		this->callbacks->errorSignal (this->error);
+		return false;
+	}
+
+}
+
+// Send a TERM signal to the process.
+void
+Process::terminate (void)
+{
+	kill (this->pid, 15); // 15 is TERM
+	// Now check if the process has gone and kill it with signal 9 (KILL)
+	this->pid = 0;
+	this->error = PROCESSNOERROR;
+	this->signalledStart = false;
+	return;
+}
+
+// Check on this process
+void
+Process::probeProcess (void)
+{
+	// Has the process started?
+	if (!this->signalledStart) {
+		if (this->pid > 0) {
+			this->callbacks->startedSignal (this->progName);
+			this->signalledStart = true;
+			dbgln ("Process::probeProcess set signalledStart and signalled the start...");
+		}
+	}
+
+	// Check for error condition
+	if (this->error>0) {
+		this->callbacks->errorSignal (this->error);
+		dbgln ("have error in probeProcess, returning");
+		return;
+	}
+
+	if (this->pid == 0) {
+		// Not yet started.
+		return;
+	}
+
+	// Why can't these 4 lines go in contructor?
+	this->p[0].fd = this->childToParent[READING_END];
+	this->p[0].events = POLLIN | POLLPRI;
+	this->p[1].fd = this->childErrToParent[READING_END];
+	this->p[1].events = POLLIN | POLLPRI;
+
+	// Poll to determine if data is available
+	this->p[0].revents = 0;
+	this->p[1].revents = 0;
+
+	poll (this->p, 2, 0);
+
+	if (this->p[0].revents & POLLNVAL || this->p[1].revents & POLLNVAL) {
+		dbgln ("Process::probeProcess: pipes closed, process must have crashed");
+		this->error = PROCESSCRASHED;
+		this->callbacks->errorSignal (this->error);
+		return;
+	}
+
+	if (this->p[0].revents & POLLIN || this->p[0].revents & POLLPRI) {
+		this->callbacks->readyReadStandardOutputSignal();
+	}
+	if (this->p[1].revents & POLLIN || this->p[1].revents & POLLPRI) {
+		this->callbacks->readyReadStandardErrorSignal();
+	}
+
+	// Is the process running? We check last, so that we get any
+	// messages on stdout/stderr that we may wish to process, such
+	// as error messages from nxssh key authentication.
+	int theError;
+	if (this->signalledStart == true) {
+		int rtn = 0;
+		if ((rtn = waitpid (this->pid, (int *)0, WNOHANG)) == this->pid) {
+			this->callbacks->processFinishedSignal (this->progName);
+			return;
+		} else if (rtn == -1) {
+			theError = errno;
+			if (theError != 10) { // We ignore errno 10 "no child" as this commonly occurs
+				cerr << "waitpid returned errno: " << theError;
+			}
+		} // else rtn == 0
+	}
+
+	return;
+}
+
+// Read stdout pipe, without blocking.
+string
+Process::readAllStandardOutput (void)
+{
+	string s;
+	int bytes = 0;
+	char c;
+	struct pollfd p;
+
+	p.fd = this->childToParent[READING_END];
+	p.events = POLLIN | POLLPRI;
+	// We know we have at least one character to read, so seed revents
+	p.revents = POLLIN;
+	while (p.revents & POLLIN || p.revents & POLLPRI) {
+		// This read of 1 byte should never block
+		if ((bytes = read (this->childToParent[READING_END], &c, 1)) == 1) {
+			s.append (1, c);
+		}
+		p.revents = 0;
+		poll (&p, 1, 0);
+	}
+	return s;
+}
+
+// Read stderr pipe without blocking
+string
+Process::readAllStandardError (void)
+{
+	string s;
+	int bytes = 0;
+	char c;
+	struct pollfd p;
+
+	p.fd = this->childErrToParent[READING_END];
+	p.events = POLLIN | POLLPRI;
+	// We know we have at least one character to read, so seed revents
+	p.revents = POLLIN;
+	while (p.revents & POLLIN || p.revents & POLLPRI) {
+		// This read of 1 byte should never block because a poll() call tells us there is data
+		if ((bytes = read (this->childErrToParent[READING_END], &c, 1)) == 1) {
+			s.append (1, c);
+		}
+		p.revents = 0;
+		poll (&p, 1, 0);
+	}
+	return s;
+}
+
+//@}
---
+++ futil/Process.h
@@ -0,0 +1,181 @@
+/*
+ * A class to fork and exec processes.
+ *
+ * Copyright 2009, William Matthew Limited.
+ *
+ * Author: S James <sjames@wmltd.co.uk>
+ */
+
+#ifndef _PROCESS_H_
+#define _PROCESS_H_
+
+#include <list>
+#include <vector>
+#include <string>
+#include <fstream>
+extern "C" {
+#include <sys/poll.h>
+}
+#define PROCESS_MAIN_APP 0
+#define PROCESS_FAILURE -1
+
+// Possible errors to be generated
+#define PROCESSNOERROR       0
+#define PROCESSFAILEDTOSTART 1
+#define PROCESSCRASHED       2
+#define PROCESSTIMEDOUT      3
+#define PROCESSWRITEERR      4
+#define PROCESSREADERR       5
+#define PROCESSUNKNOWN       6
+
+using namespace std;
+
+#ifdef DEBUG
+extern ofstream debugLogFile;
+# define dbgln(msg)  debugLogFile << __FUNCTION__ << ": " << msg << endl;
+# define dbglln(msg) debugLogFile << __PRETTY_FUNCTION__ << ": " << msg << endl;
+# define dbg(msg)    debugLogFile << msg;
+#else
+# define dbgln(msg)
+# define dbglln(msg)
+# define dbg(msg)
+#endif
+
+namespace wml {
+
+	/*!
+	 * A set of virtual callbacks. These should be derived in the
+	 * client code. They're called by Process via the
+	 * ProcessCallbacks* callbacks member variable.
+	 */
+	class ProcessCallbacks
+	{
+	public:
+		ProcessCallbacks() {}
+		virtual ~ProcessCallbacks() {}
+		virtual void startedSignal (string) {}
+		virtual void errorSignal (int) {}
+		virtual void processFinishedSignal (string) {}
+		virtual void readyReadStandardOutputSignal (void) {}
+		virtual void readyReadStandardErrorSignal (void) {}
+	};
+
+	/*!
+	 * Process is a simple replacement for the Qt class QProcess.
+	 */
+	class Process
+	{
+	public:
+		Process();
+		~Process();
+
+		/*!
+		 * Write \arg input to the stdin of the process.
+		 */
+		void writeIn (string& input);
+
+		/*!
+		 * fork and exec the process.
+		 */
+		int start (const string& program, const list<string>& args);
+
+		/*!
+		 * Send a TERM signal to the process.
+		 */
+		void terminate (void);
+
+		/*!
+		 * poll to see if there is data on stderr or stdout
+		 * and to see if the process has exited.
+		 *
+		 * This must be called on a scheduled basis. It checks
+		 * for any stdout/stderr data and also checks whether
+		 * the process is still running.
+		 */
+		void probeProcess (void);
+
+		/*!
+		 * Accessors
+		 */
+		//@{
+		pid_t getPid (void) { return this->pid; }
+		int getError (void) { return this->error; }
+		void setError (int e) { this->error = e; }
+
+		/*!
+		 * Setter for the callbacks.
+		 */
+		void setCallbacks (ProcessCallbacks * cb) { this->callbacks = cb; }
+		//@}
+
+		/*!
+		 * Slots
+		 */
+		//@{
+		string readAllStandardOutput (void);
+		string readAllStandardError (void);
+
+		/*!
+		 * Wait for the process to get itself going. Do this
+		 * by looking at pid.  If no pid after a while,
+		 * return false.
+		 */
+		bool waitForStarted (void);
+		//@}
+	private:
+		/*!
+		 * The name of the program to execute
+		 */
+		string progName;
+
+		/*!
+		 * The environment and arguments of the program to execute
+		 */
+		list<string> environment;
+
+		/*!
+		 * Holds a Process error, defined above. PROCESSNOERROR, etc.
+		 */
+		int error;
+
+		/*!
+		 * Process ID of the program
+		 */
+		pid_t pid;
+
+		/*!
+		 * Set to true if the fact that the program has been
+		 * started has been signalled using the callback
+		 * callbacks->startedSignal
+		 */
+		bool signalledStart;
+
+		/*!
+		 * stdin parent to child
+		 */
+		int parentToChild[2];
+
+		/*!
+		 * stdout child to parent
+		 */
+		int childToParent[2];
+
+		/*!
+		 * stderr child to parent
+		 */
+		int childErrToParent[2];
+
+		/*!
+		 * Used in the poll() call in probeProcess()
+		 */
+		struct pollfd * p;
+
+		/*!
+		 * Pointer to a callback object
+		 */
+		ProcessCallbacks * callbacks;
+	};
+
+} // namespace wml
+
+#endif // _PROCESS_H_
